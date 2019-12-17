6352d9ca78c12739350db552580ea39e396a61b2
Ralph Gutkowski <rgtk@protonmail.com>
2019-12-17 23:57:00 +0100

    Support splitting window horizontally

--- spacious-mode.el
+++ spacious-mode.el
@@ -1,11 +1,18 @@
-(defun spacious/adjust-windows ()
-  (interactive)
+(defun spacious-mode--reset-window (&optional window)
+  (set-window-margins (or window (get-buffer-window)) 0 0))
+
+(defun spacious-mode--adjust-windows ()
   (walk-windows
    (lambda (window)
      (unless (window-minibuffer-p window)
        (let ((margin (floor (/ (- (window-total-width window) 80) 2))))
          (set-window-margins window margin margin))))))
 
+(defun spacious-mode-on ()
+  (spacious-mode +1))
+
+(defun spacious-mode-off (&optional any)
+  (spacious-mode -1))
 
 ;; TODO walk all touched windows
 (defun spacious-mode (&optional toggle)
@@ -21,12 +28,14 @@
       (progn
         (setq spacious-mode nil)
 
-        (remove-hook 'window-configuration-change-hook 'spacious/adjust-windows)
+        (remove-hook 'window-configuration-change-hook 'spacious-mode--adjust-windows)
+        (remove-hook 'window-setup-hook 'spacious-mode-on)
+        (advice-remove 'split-window-right 'spacious-mode--reset-window)
 
         (walk-windows
          (lambda (window)
            (unless (window-minibuffer-p window)
-             (set-window-margins window 0 0))))
+             (spacious-mode--reset-window window))))
 
         (message "Spacious mode disabled"))
 
@@ -35,7 +44,10 @@
 
       (progn
         (setq spacious-mode t)
-        (add-hook 'window-configuration-change-hook 'spacious/adjust-windows)
+
+        (add-hook 'window-configuration-change-hook 'spacious-mode--adjust-windows)
+        (add-hook 'window-setup-hook 'spacious-mode-on)
+        (advice-add 'split-window-right :before 'spacious-mode--reset-window)
 
         (let* ((win (get-buffer-window))
                (content-width 80)
@@ -44,15 +56,5 @@
           (set-window-margins win margin margin))
 
         (message "Spacious mode enabled")))))
-
-(defun spacious-mode-on ()
-  (spacious-mode +1))
-
-(defun spacious-mode-off (&optional any)
-  (spacious-mode -1))
-
-(defun spacious-setup ()
-  (interactive)
-  (add-hook 'window-setup-hook 'spacious-mode-on))
 
 (provide 'spacious-mode)
